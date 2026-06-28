b9327ba38309f5cf9c7295acb6b80ddb9593be04
MUHAMMAD RAHEEL_SHAKEEL <raheelshakeel953@gmail.com>
2026-06-28 07:07:42 +0000

    Create src/utils.py

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+"""
+Utility functions.
+"""
+import logging
+from rich.logging import RichHandler
+
+def setup_logging():
+    logging.basicConfig(
+        level="INFO",
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[RichHandler(rich_tracebacks=True)]
+    )
